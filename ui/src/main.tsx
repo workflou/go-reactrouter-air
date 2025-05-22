2204de33739a6bd8ce0424461017c3bb8fc5b507
Krzysztof <krzysztof@engineer.com>
2025-05-22 15:47:17 +0200

    add react router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Link, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>Hello, world</div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Hello, world! <Link to="/about">About</Link></div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
