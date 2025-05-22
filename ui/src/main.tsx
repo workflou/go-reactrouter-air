795687193b0ed0d7ff69b1234daa26cdc58a039a
Krzysztof <krzysztof@engineer.com>
2025-05-22 15:49:48 +0200

    add mantine ui

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Link, Route, Routes } from 'react-router'
+import '@mantine/core/styles.css';
+import { Button, MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<div>Hello, world! <Link to="/about">About</Link></div>} />
-        <Route path="/about" element={<div>About</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
-      </Routes>
-    </HashRouter>
+    <MantineProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<div>Hello, world! <Link to="/about">About</Link></div>} />
+          <Route path="/about" element={<Button variant="filled">Button</Button>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </HashRouter>
+    </MantineProvider>
   </StrictMode>,
 )
